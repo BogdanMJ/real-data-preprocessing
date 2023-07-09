c489b4e9fcd728a201b6c19541899f4e68452cef
BogdanMJ <129210491+BogdanMJ@users.noreply.github.com>
2023-07-09 13:06:15 +0200

    Add files via upload

--- real-data-preprocessing.py
+++ real-data-preprocessing.py
@@ -3,43 +3,43 @@ import numpy
 
 data = pandas.read_csv('EURUSD_H4.csv')
 df = pandas.DataFrame(data)
-data_2500 = df.head(2500) # Wyciągnięcie pierwszsych 2500 elementów
+data_2500 = df.head(2500) # Extraction of the first 2500 items
 
-del data_2500["SMA14IND"] # usunięcie całej kolumny
-del data_2500["SMA50IND"] # usunięcie całej kolumny
+del data_2500["SMA14IND"] # remove the entire column
+del data_2500["SMA50IND"] # remove the entire column
 
-pusteCLOSE = data_2500['Close'].isna().sum() #Zliczanie elementów pustych w kolumnie CLOSE
+emptyCLOSE = data_2500['Close'].isna().sum() # Counting empty elements in the CLOSE column
 
-data_2500['Close'] = data_2500['Close'].interpolate() # uzupełnienie pustych danych o srednią z 2 sąsiednich
-data_2500['SMA14'] = data_2500['SMA14'].interpolate() # uzupełnienie pustych danych o srednią z 2 sąsiednich
-data_2500['SMA50'] = data_2500['SMA50'].interpolate() # uzupełnienie pustych danych o srednią z 2 sąsiednich
+data_2500['Close'] = data_2500['Close'].interpolate() # filling empty data with an average of 2 neighboring ones
+data_2500['SMA14'] = data_2500['SMA14'].interpolate() # filling empty data with an average of 2 neighboring ones
+data_2500['SMA50'] = data_2500['SMA50'].interpolate() # filling empty data with an average of 2 neighboring ones
 
-data_2500['Bulls'] = data_2500['Bulls'].fillna(0) # Uzupełnia wartoci puste liczbą 0
-data_2500['CCI'] = data_2500['CCI'].fillna(0) # Uzupełnia wartoci puste liczbą 0
-data_2500['DM'] = data_2500['DM'].fillna(0) # Uzupełnia wartoci puste liczbą 0
-data_2500['OSMA'] = data_2500['OSMA'].fillna(0) # Uzupełnia wartoci puste liczbą 0
-data_2500['RSI'] = data_2500['RSI'].fillna(0) # Uzupełnia wartoci puste liczbą 0
-data_2500['Stoch'] = data_2500['Stoch'].fillna(0) # Uzupełnia wartoci puste liczbą 0
+data_2500['Bulls'] = data_2500['Bulls'].fillna(0) # Filling blank values with 0
+data_2500['CCI'] = data_2500['CCI'].fillna(0) # Filling blank values with 0
+data_2500['DM'] = data_2500['DM'].fillna(0) # Filling blank values with 0
+data_2500['OSMA'] = data_2500['OSMA'].fillna(0) # Filling blank values with 0
+data_2500['RSI'] = data_2500['RSI'].fillna(0) # Filling blank values with 0
+data_2500['Stoch'] = data_2500['Stoch'].fillna(0)# Filling blank values with 0
 
-kor1 = data_2500['Close'].corr(data_2500['SMA14']) # korelacja między kolumnami
-kor2 = data_2500['Close'].corr(data_2500['SMA50'])
-większa_korelacja = max(kor1, kor2)
+kor1 = data_2500['Close'].corr(data_2500['SMA14']) # correlation between columns
+kor2 = data_2500['Close'].corr(data_2500['SMA50']) # correlation between columns
+greater_correlation = max(kor1, kor2)
 
-# usunięcie kolumny o większej korelacji
+# delete column with higher correlation
 if kor1 < kor2:
     del data_2500["SMA50"]
 if kor1 > kor2:
     del data_2500["SMA14"]
 
 
-#Wyciąganie wartoci ujemnych z CCI
-ujemne_CCI = []
+#Extracting negative values from CCI
+negative_CCI = []
 for el in data_2500['CCI']:
     if el < 0 :
-        ujemne_CCI.append(el)
-print(f"Iloć ujemnych atrybutów dla CCI wynosi: {len(ujemne_CCI)}") 
+        negative_CCI.append(el)
+print(f"The number of negative attributes for CCI is: {len(negative_CCI)}") 
 
-#Podanie min i max dla pozostałych atrybutów -> ??
+# Specifying min and max for other attributes
 
 min_Close = data_2500['Close'].min()
 max_Close = data_2500['Close'].max()
@@ -50,43 +50,43 @@ max_SMA50 = data_2500['SMA50'].max()
 min_Bulls = data_2500['Bulls'].min()
 max_Bulls = data_2500['Bulls'].max()
 
-#Normalizacja danych dla kolumn Close i SMA50
-atrybuty = ['Close','SMA50']
-for atrybut in atrybuty:
-    max = data_2500[atrybut].max()
-    min = data_2500[atrybut].min()
-    data_2500[atrybut] = (data_2500[atrybut] - min) / (max - min)
+#Data normalization for Close and SMA50 columns
+attributes = ['Close','SMA50']
+for attribute in attributes:
+    max = data_2500[attribute].max()
+    min = data_2500[attribute].min()
+    data_2500[attribute] = (data_2500[attribute] - min) / (max - min)
 
 """
 seria = pandas.cut(pandas.Series(numpy.array(data_2500['Bulls'])),2,labels=['Lower','Upper'])
 data_2500['Bulls'].hist()
 """
 
-data_2500['Bulls'].hist() # histogram przed dyskretyzacją
+data_2500['Bulls'].hist() # histogram before discretization
 
-# histogram po dyskretyzacji
-etykiety = ['Up','Down']
-seria = pandas.Series(numpy.array(data_2500['Bulls']))
-wynik = pandas.cut(seria,2,labels = etykiety)  
-wynik.hist()
+# histogram after discretization
+labels = ['Up','Down']
+series = pandas.Series(numpy.array(data_2500['Bulls']))
+result = pandas.cut(series,2,labels = labels)  
+result.hist()
 
 
-data_2500['Close'].hist() # histogram przed dyskretyzacją
+data_2500['Close'].hist() # histogram before discretization
 
-# histogram po dyskretyzacji
-etykietyClose = ['0,0 - 0,25','0,26 - 0,5','0,51 - 0,75','0,76 - 1,0']
-seriaClose = pandas.Series(numpy.array(data_2500['Close']))
-wynikClose = pandas.cut(seriaClose,4,labels = etykietyClose)
-wynikClose.hist()
+# histogram after discretization
+labelsClose = ['0,0 - 0,25','0,26 - 0,5','0,51 - 0,75','0,76 - 1,0']
+seriesClose = pandas.Series(numpy.array(data_2500['Close']))
+resultClose = pandas.cut(seriesClose,4,labels = labelsClose)
+resultClose.hist()
 
-#Rysowanie wykresu kołowego
+# Drawing a pie chart
 data_2500['Decision'].value_counts().plot.pie()
 
-#rysowanie wykresu liniowego 
+# Drawing a line graph
 
 data_2500['Close'].plot()
 
 
-print(f"Puste z kolumy Close: {pusteCLOSE}")
-print(f"Większa Korelacja dla: {większa_korelacja}")
+print(f"Empty from the Close column: {emptyCLOSE}")
+print(f"Greater Correlation for: {greater_correlation}")
 print(data_2500)
